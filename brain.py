2190e457acd36ae6972aa5a6b45282130823d643
aahsancmu <152228389+aahsancmu@users.noreply.github.com>
2023-11-28 01:17:15 -0500

    Update brain.py

--- brain.py
+++ brain.py
@@ -9,17 +9,17 @@ from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe
 import pandas as pd
 from langchain.llms import OpenAI
 
-llm = ChatOpenAI(openai_api_key=os.environ["OPENAI_API_KEY"])
+# llm = ChatOpenAI(openai_api_key=os.environ["OPENAI_API_KEY"])
 
 def get_me_an_answer(question, key):
   # llm = ChatOpenAI(openai_api_key=key)
   df = pd.read_csv("Neosporin_Patients.csv")
 
   agent = create_pandas_dataframe_agent(
-      ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613", openai_api_key=os.environ["OPENAI_API_KEY"],
+      ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613", openai_api_key=os.environ["OPENAI_API_KEY"]),
       df,
       verbose=True,
-      agent_type=AgentType.OPENAI_FUNCTIONS,
+      agent_type=AgentType.OPENAI_FUNCTIONS
   )
 
   return agent.run(question)
